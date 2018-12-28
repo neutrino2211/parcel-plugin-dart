cab0334f8a4551f87cc7df891795ffab23ce7fdc
neutrino2211 <neutrino2211@gmail.com>
2018-12-28 23:59:31 +0100

    [Test]: Run chrome in headless mode

--- test/index.js
+++ test/index.js
@@ -2,12 +2,13 @@ const fileUrl = require("file-url")
 let uriNode8 = fileUrl("test/basic/build/index.html")
 let uriNodePre8 = fileUrl("test/basic/build-node-pre-8/index.html")
 const {Builder, By} = require('selenium-webdriver');
+const {Options} = require('selenium-webdriver/chrome')
 const assert = require("assert")
 
 describe("Basic project page",function(){
     this.slow(20000)
     this.timeout(30000)
-    let browser = new Builder().forBrowser('chrome');
+    let browser = new Builder().forBrowser('chrome').setChromeOptions(new Options().headless())
     let driver = browser.build();
     describe("Node > 8",()=>{
         it("should have #output text as 'Your Dart app is running.'",async()=>{

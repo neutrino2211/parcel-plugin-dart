a67c375211c92b9c262143e3e8be2dc96237834f
neutrino2211 <neutrino2211@gmail.com>
2018-12-28 03:47:10 +0100

    [cc]:8

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -1,11 +1,10 @@
-NODE=node
-
-if ! type "$NODE" > /dev/null; then
-  sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+NODE=$(command -v node)
+if [ "$NODE" == "node" ]; then
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
   export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-    sudo nvm install --lts=carbon
+    nvm install --lts=carbon
 fi
 
-sudo npm install parcel-bundler -g
-sudo npm run test
+npm install parcel-bundler -g
+npm run test
